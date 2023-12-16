27a0757bfca8513e613f9483283d0d3c7b04184f
WanAdamm <wanadamworkmail@gmail.com>
2023-12-16 13:56:50 +0800

    added some file for preprocessing

--- sortFileByInitials.cpp
+++ sortFileByInitials.cpp
@@ -6,12 +6,34 @@ using namespace std;
 
 int main()
 {
-    fstream outputFile;
+    ifstream outputFile;
+    ofstream sortedOutputFile;
+    ofstream outputFileWithAsciiSum;
+
     outputFile.open("outputFile.txt");
+    sortedOutputFile.open("sortedOutputFile.txt");
+    outputFileWithAsciiSum.open("outputFileWithAsciiSum.txt");
     string word, meaning, inputFileName, outputFileName;
     string index, wordInitial, searchedWord, all;
+    int asciiSum;
 
     // TODO: add sorting word capabilities (sort by alphabet????)
-    
+
+    while (getline(outputFile, index, '\t'))
+    {
+        getline(outputFile, word, '\t');
+        getline(outputFile, all);
+
+        for (auto &ch : word)
+        {
+            cout << ch << endl;
+            asciiSum += ch;
+        }
+
+        outputFileWithAsciiSum << index << '\t' << word << '\t' << asciiSum << '\t' << all << endl;
+
+        asciiSum = 0;
+    }
+
     return 0;
 }
