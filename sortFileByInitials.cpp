fc90ee1691b77d2c0d66649a9053b48b2fd12c57
WanAdamm <wanadamworkmail@gmail.com>
2023-12-18 10:22:23 +0800

    unsuccessful approach

--- sortFileByInitials.cpp
+++ sortFileByInitials.cpp
@@ -8,16 +8,16 @@ int main()
 {
     ifstream outputFile;
     ofstream sortedOutputFile;
-    ofstream outputFileWithAsciiSum;
+    fstream outputFileWithAsciiSum;
 
     outputFile.open("outputFile.txt");
     sortedOutputFile.open("sortedOutputFile.txt");
     outputFileWithAsciiSum.open("outputFileWithAsciiSum.txt");
     string word, meaning, inputFileName, outputFileName;
-    string index, wordInitial, searchedWord, all;
+    string index, wordInitial, searchedWord, all, firstWordAsciiValue;
+    string indexSelected, wordSelected, firstWordAsciiValueSelected, allSelected;
+    string tempWord, tempFirstWordAsciiValueSelected, tempAllSelected;
     int asciiFirstWord;
-
-    // TODO: add sorting word capabilities (sort by alphabet????)
 
     while (getline(outputFile, index, '\t'))
     {
@@ -29,6 +29,31 @@ int main()
         outputFileWithAsciiSum << index << '\t' << word << '\t' << asciiFirstWord << '\t' << all << endl;
 
         asciiFirstWord = 0;
+    }
+
+    // reset file pointer
+    outputFileWithAsciiSum.clear();
+    outputFileWithAsciiSum.seekp(0);
+
+    // TODO: add sorting word capabilities (sort by alphabet????)
+
+    getline(outputFileWithAsciiSum, indexSelected, '\t');
+    getline(outputFileWithAsciiSum, wordSelected, '\t');
+    while (getline(outputFileWithAsciiSum, firstWordAsciiValueSelected, '\t'))
+    {
+        getline(outputFileWithAsciiSum,  allSelected);
+
+        while(getline(outputFileWithAsciiSum, index, '\t'))
+        {
+            getline(outputFileWithAsciiSum, word, '\t');
+            getline(outputFileWithAsciiSum, firstWordAsciiValue, '\t');
+            getline(outputFileWithAsciiSum, all);
+
+            if(firstWordAsciiValueSelected > firstWordAsciiValue)
+            {
+                
+            }
+        }
     }
 
     return 0;
